24f767175630aa4c877be95b6b62846d94330d30
Oscar18900 <147523781+Oscar18900@users.noreply.github.com>
2024-08-31 08:27:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+// Clase Contacto
+class Contacto {
+public:
+    string nombre;
+    string apellidos;
+    string correo;
+    string celular;
+    string fechaNacimiento;
+
+    Contacto(string n, string a, string c, string ce, string f) :
+        nombre(n), apellidos(a), correo(c), celular(ce), fechaNacimiento(f) {}
+};
